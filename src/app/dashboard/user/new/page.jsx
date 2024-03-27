4fe4b2377d7958ddeb800f31d35389fdda106e7f
Ebisa Girma <ebisagirma41@gmail.com>
2024-03-27 07:52:23 +0300

    chat and some styles

--- src/app/dashboard/user/new/page.jsx
+++ src/app/dashboard/user/new/page.jsx
@@ -1,10 +1,36 @@
 "use client";
 import NewDeliveryPersonnelForm from "@/components/dashboard/user/new-dp-form";
 import React from "react";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import Link from "next/link";
 
 const NewUser = () => {
   return (
     <div>
+      <div className="mb-2">
+        <div className="text-xl my-2 font-bold "> User Creation</div>
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <Link href="/dashboard">Dashboard</Link>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <Link href="/dashboard/user">User</Link>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage>User Creation</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
       <NewDeliveryPersonnelForm />
     </div>
   );
--- src/app/dashboard/user/update/[id]/page.jsx
+++ src/app/dashboard/user/update/[id]/page.jsx
@@ -2,10 +2,36 @@
 
 import UpdateDeliveryPersonnelForm from "@/components/dashboard/user/update-dp-form";
 import React from "react";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import Link from "next/link";
 
 const UpdateUser = ({ params }) => {
   return (
     <div>
+      <div className="mb-2">
+        <div className="text-xl my-2 font-bold "> User Update</div>
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <Link href="/dashboard">Dashboard</Link>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <Link href="/dashboard/user">User</Link>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage>User Update</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
       <UpdateDeliveryPersonnelForm userId={params.id} />
     </div>
   );
