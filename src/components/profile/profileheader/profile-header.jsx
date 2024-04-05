e914f855db25983676d2b1a8e7d9f426b4585201
Ebisa Girma <ebisagirma41@gmail.com>
2024-04-05 16:35:06 +0300

    chat ad profile

--- src/components/profile/profileheader/profile-header.jsx
+++ src/components/profile/profileheader/profile-header.jsx
@@ -9,16 +9,18 @@ const ProfileHeader = ({ name, bio, profilePicture, backgroundImage }) => {
           alt={`${name}'s profile picture`}
           width={80}
           height={80}
-          className=" size-28 rounded-full object-cover"
+          className=" size-28 rounded-full object-cover z-50"
         />
         {name}
       </div>
-      <Image
-        className="w-full size-64 object-cover bg-black "
-        width={1500}
-        height={1500}
-        src={"/banner.jpg"}
-      />
+      <div className="bg-jade  rounded-sm">
+        <Image
+          className="w-full size-64 object-cover rounded-sm "
+          width={1500}
+          height={1500}
+          src={"/banner.png"}
+        />
+      </div>
 
       <div className=""></div>
     </div>
