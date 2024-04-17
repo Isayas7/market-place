793068ff05450c2f0b9e494b15c50270190c111b
Ebisa Girma <ebisagirma41@gmail.com>
2024-04-17 09:21:46 +0300

    sockect for chat

---
+++ socket/index.js
@@ -0,0 +1,43 @@
+const io = require("socket.io")(8900, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+let users = [];
+
+const addUser = (user, socketId) => {
+  !users.some((myUser) => myUser.user.id === user.id) &&
+    users.push({ user, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.user.id === userId);
+};
+
+io.on("connection", (socket) => {
+  // take user Id and socket Id from user
+  socket.on("addUser", (user) => {
+    addUser(user, socket.id);
+    io.emit("getUsers", users);
+  });
+
+  //   disconnection
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
+
+  //   send AND get message
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId);
+    io.to(user.socketId).emit("getMessage", {
+      senderId,
+      text,
+    });
+  });
+});
--- src/components/queryprovider/query-provider.js
+++ src/components/queryprovider/query-provider.js
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const QueryProvider = ({ children }) => {
   const queryClient = new QueryClient({
