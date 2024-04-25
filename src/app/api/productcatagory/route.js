d4d209e880fa4eb98bd54ed64722d56a43d16d8b
Isayas <isayas2024@gmail.com>
2024-04-25 12:15:17 +0300

    pagination implementation started

--- src/app/api/productcatagory/route.js
+++ src/app/api/productcatagory/route.js
@@ -5,13 +5,25 @@ import { NextResponse } from "next/server";
 export const GET = async (request) => {
   const { searchParams } = new URL(request.url);
   let query = {};
-
   searchParams.forEach((value, key) => {
     query[key] = value;
   });
+
+  delete query.page;
+
+  const pageSize = 1;
+  const pageNumber = parseInt(query.page);
+  const count = await ProductCategory.find(query).countDocuments();
+
   try {
     await connect();
-    const productCategories = await ProductCategory.find(query);
+    const productCategories = await ProductCategory.find(query)
+      .limit(pageSize)
+      .skip((pageNumber - 1) * pageSize);
+
+    const totalPage = Math.ceil(count / pageSize);
+
+    productCategories.totalPage = totalPage;
 
     return new NextResponse(JSON.stringify(productCategories), { status: 200 });
   } catch (error) {
