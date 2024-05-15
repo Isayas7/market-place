c458520bce1b55e077f5cdb2459bff9d5e5e1aca
Isayas <100583517+Isayas7@users.noreply.github.com>
2024-05-15 18:29:31 +0300

    Update data_table.jsx

--- src/components/dashboard/table/data_table.jsx
+++ src/components/dashboard/table/data_table.jsx
@@ -304,8 +304,7 @@ export function DataTable({
           </div>
           <div className=" flex justify-between items-center">
             <div className="flex-1 text-sm text-muted-foreground">
-              {table.getFilteredSelectedRowModel().rows.length} of{" "}
-              {table.getFilteredRowModel().rows.length} row selected.
+             
             </div>
             <div className="flex items-center justify-end space-x-2 py-4">
               <span>
